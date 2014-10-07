e99d42233c43ca83a421365ca79596c504b48c87
Flint LaCour <flint@fortifydesign.com>
2014-10-06 21:07:13 -0500

    updated in class js exercises

---
+++ wk4_array_loops/partner_exercise_3/exercise.js
@@ -0,0 +1,18 @@
+// event listener
+
+$('document').on('click', 'button', function(){
+	console.log('clicked!');
+});
+
+// or
+
+$('button').on('click', 'button', function(){
+
+});
+
+
+
+$('button').val('button').appendTo('body'{
+
+});
+
